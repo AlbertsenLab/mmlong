6491a7de9affa1d249754fe700f7033931fa6ad7
SorenKarst <sorenkarst@gmail.com>
2018-02-01 11:29:52 +0100

    Created dependencies.sh
    Created dependencies.sh, which contains the paths to all the
    dependencies used by the mmlong package. The package is sourced by the
    individual tools.

---
+++ mmlong_tools/dependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# mmlong dependencies
+# By Søren Karst
+# Version
+VERSION=0.1.0
+
+# Paths to dependencies
+FILTLONG=/space/sharedbin/bin/filtlong;
+CUTADAPT=/space/users/smk/bin/cutadapt;
+PORECHOP=/space/users/smk/bin/porechop; 
+RACON=/space/sharedbin/bin/racon;
+MINIASM=/space/sharedbin/bin/miniasm;
+MINIMAP2=/space/users/smk/Software/minimap2-2.5/minimap2-2.5_x64-linux/minimap2;
+SAMTOOLS=/space/users/smk/Software/SAMtools/bin/samtools;
+PARALLEL=/usr/bin/parallel;
+PRODIGAL=/space/sharedbin/bin/prodigal;
+HMMSEARCH=/usr/bin/hmmsearch;
+ESSENTIAL=/space/users/smk/Software/databases/essential/essential.hmm;
+KAIJU=/space/users/smk/Software/kaiju/bin;
+KAIJU_DB=/space/users/smk/Software/kaiju/database;
+BARRNAP=/space/users/smk/Software/barrnap-0.8/bin/barrnap;
+MOTHUR=/space/sharedbin/bin/mothur;
+MOTHUR_DB=/space/users/smk/Software/databases/mothur/silva.seed_v132.align;
+MOTHUR_TAX=/space/users/smk/Software/databases/mothur/silva.seed_v132.tax;
+RSCRIPT=/usr/bin/Rscript;
