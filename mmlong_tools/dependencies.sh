c9f5ad91767f8ef20f69709dc723de2db713b5af
SorenKarst <sorenkarst@gmail.com>
2018-08-06 14:56:32 +0200

    Add and update dependencies

--- mmlong_tools/dependencies.sh
+++ mmlong_tools/dependencies.sh
@@ -2,34 +2,42 @@
 # mmlong dependencies
 # By Søren Karst
 # Version
-VERSION=0.1.1
+VERSION=0.1.4
 
 ### Paths to dependencies
+# mmlong
+MMLONG_LINKS=/space/users/smk/Desktop/2017_long_read/pipeline/mmlong/mmlong_tools/mmlong_links;
+MMLONG_READCOVERAGE=/space/users/smk/Desktop/2017_long_read/pipeline/mmlong/mmlong_tools/mmlong_readcoverage;
+MMLONG_MINIASM_RACON=/space/users/smk/Desktop/2017_long_read/pipeline/mmlong/mmlong_tools/mmlong_miniasm-racon;
 
 # General
 FILTLONG=/space/sharedbin/bin/filtlong;
-RACON=/space/sharedbin/bin/racon;
+RACON=/space/users/smk/Software/racon/build/bin/racon;
 MINIASM=/space/sharedbin/bin/miniasm;
-MINIMAP2=/space/users/smk/Software/minimap2-2.5/minimap2-2.5_x64-linux/minimap2;
+MINIMAP2=//space/users/smk/Software/minimap2-2.11/minimap2;
 SAMTOOLS=/space/users/smk/Software/SAMtools/bin/samtools;
 PARALLEL=/usr/bin/parallel;
+SPADES=/space/users/smk/Software/Spades3.11/SPAdes-3.11.1-Linux/bin/spades.py;
 
 # Metaflow
-CUTADAPT=/space/users/smk/bin/cutadapt;
-PORECHOP=/space/users/smk/bin/porechop; 
+CUTADAPT=/usr/local/bin/cutadapt;
+PORECHOP=/space/users/smk/bin/porechop;
 PRODIGAL=/space/sharedbin/bin/prodigal;
+FRAGGENESCAN=/space/users/smk/Software/FragGeneScan1.30/run_FragGeneScan.pl;
 HMMSEARCH=/usr/bin/hmmsearch;
-ESSENTIAL=/space/users/smk/Software/databases/essential/essential.hmm;
+ESSENTIAL=/space/users/smk/Desktop/2017_long_read/pipeline/mmlong/mmlong_tools/databases/essential.hmm;
 KAIJU=/space/users/smk/Software/kaiju/bin;
 KAIJU_DB=/space/users/smk/Software/kaiju/database;
 BARRNAP=/space/users/smk/Software/barrnap-0.8/bin/barrnap;
 MOTHUR=/space/sharedbin/bin/mothur;
-MOTHUR_DB=/space/users/smk/Software/databases/mothur/silva.seed_v132.align;
-MOTHUR_TAX=/space/users/smk/Software/databases/mothur/silva.seed_v132.tax;
+MOTHUR_DB=/space/users/smk/Desktop/2017_long_read/pipeline/mmlong/mmlong_tools/databases/silva.seed_v132.align;
+MOTHUR_TAX=/space/users/smk/Desktop/2017_long_read/pipeline/mmlong/mmlong_tools/databases/silva.seed_v132.tax;
 RSCRIPT=/usr/bin/Rscript;
+METABAT2_COV=/space/sharedbin/bin/jgi_summarize_bam_contig_depths;
+METABAT2=/space/sharedbin/bin/metabat2;
+MEGAHIT=/space/users/smk/bin/megahit;
 
 # Reassembly
-SPADES=/space/users/smk/Software/Spades3.11/SPAdes-3.11.1-Linux/bin/spades.py;
 UNICYCLER=/space/users/smk/bin/unicycler;
 
 # Stats
